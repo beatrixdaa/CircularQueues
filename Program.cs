84481f52ae6a3dff6ee96e1f9e866a481caa468f
Beatrix Devanti <beatrix.devanti.ft21@mail.umy.ac.id>
2022-12-16 15:24:58 +0700

    add public queues

--- Program.cs
+++ Program.cs
@@ -10,5 +10,19 @@ namespace CircularQueues
     {
         int FRONT, REAR, max = 5;
         int[] queue_array = new int[5];
+        public Queues()
+        {
+            /* Initial the values of the variabel REAR and FRONT to -1 
+             *  to indicate the queue is initially empty.*/
+            FRONT = -1;
+            REAR = -1;
+
+        }
+        public void insert(int element)
+        {
+            /* This statment check for the overflow condition. */
+
+        }
     }
 }
+   
